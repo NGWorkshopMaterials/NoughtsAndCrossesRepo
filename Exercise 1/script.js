4e15383f3980c2f5e1691d337d9e471cbf26b069
NGWorkshopMaterials <131272530+NGWorkshopMaterials@users.noreply.github.com>
2023-04-25 15:05:04 +0100

    Add files via upload

---
+++ Exercise 1/script.js
@@ -0,0 +1,29 @@
+
+
+
+
+let circleTurn
+
+startGame()
+
+restartButton.addEventListener('click', startGame)
+
+function startGame() {
+
+}
+
+function handleClick(e) {
+
+}
+
+
+
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+
+
+function checkWin(currentClass) {
+  
+}
